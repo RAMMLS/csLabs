8083b00a41a0a4a74c528aec4f6780817e1c71fe
RAMMLS <robertr.shakirov@bk.ru>
2025-10-13 17:31:18 +0300

    Written build_and_run.sh

---
+++ labInUniver/lab_8/build_and_run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Скрипт для сборки и запуска MouseTracker в Docker
+
+echo "===Building docker image==="
+docker build -t mouse-tracker .
+
+echo "===Starting container==="
+# Запуск контейнера с пробросом X11 для отображения GUI
+
+docker run -it --tm \
+  -e DISPLAY = $DISPLAY \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  --name mouse-tracker-app \
+  mouse-tracker
